f3f92cdb8c0bac6aeb7008ef44750c2d3e0deee1
sharad <sharadshetty2016@gmail.com>
2021-05-16 00:06:54 +0530

    create file using fs

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,46 @@
+const fs = require('fs');
+const distinctColors = require('distinct-colors').default;
+const path = require('path');
+
 const createControllers = () => {
   const controllers = {
     status: async (req, res) => {
       try {
         res.send("Welcome to Memory game's API.");
       } catch (err) {
+        console.log('🚀 ~ file: index.js ~ line 11 ~ status: ~ err', err);
+        res.status(400).send({ err });
+      }
+    },
+
+    //for initiating new game
+    newGame: async (req, res) => {
+      try {
+        const { difficulty } = req.body;
+        if (!difficulty) {
+          return res.status(400).send('difficulty is required');
+        }
+        const palette = distinctColors();
+        const objPalette = {
+          palette: palette,
+        };
+        const content = JSON.stringify(objPalette);
+        const appDir = path.dirname(require.main.filename).split('/');
+        const filePath = `/${appDir[1]}/${appDir[2]}/game-boards`;
+        //create directory if it does not exists
+        fs.mkdir(filePath, { recursive: true }, (err) => {
+          if (err) throw err;
+        });
+        //write the data to file
+        fs.writeFile(`${filePath}/${Date.now()}.json`, content, (err) => {
+          if (err) {
+            if (err) throw err;
+          }
+        });
+
+        res.send({ palette });
+      } catch (err) {
+        console.log('🚀 ~ file: index.js ~ line 23 ~ newGame: ~ err', err);
         res.status(400).send({ err });
       }
     },
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const registerRoutes = (app, controllers) => {
   router.get('/status', controllers.status);
+  router.post('/game/new', controllers.newGame);
 
   app.use('/', router);
 };
